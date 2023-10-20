7ebf79337f6807f76da7902965c27dedba7bd403
policium <ito.shogo@gmail.com>
2023-10-20 09:43:49 +0900

    add grin_one

---
+++ keyboards/grin_keebs/grin_one/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x30, 0x9F, 0xCF, 0x52, 0x8E, 0x40, 0xE2, 0x73}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 1, 8 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 4 }
